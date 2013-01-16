dc8476e149374ec2a6f0e48d5602fb4d43375a79
Grant Regimbal <regimbal.grant@gmail.com>
2013-01-16 13:19:44 -0800

    added "REST"

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 from flask import Flask
 from flask import render_template
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext import restful
 
 app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
 db = SQLAlchemy(app)
+api = restful.Api(app)
 
 @app.route("/")
 def index():
 	return render_template("index.html")
 
+
+#Rest Api
+class HelloWorld(restful.Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/api')
 
 if __name__ == '__main__':
 	app.run()
