51c9e67e73d2994178b5b9fd79339ff78d69a5f0
Grant Regimbal <regimbal.grant@gmail.com>
2013-03-16 14:17:50 -0700

    key gen and removed sql_sqlite

--- setup.py
+++ setup.py
@@ -1,2 +1,13 @@
-import setup_sqlite.py
-#more to come in the future
+import os
+key = "'" + str(os.urandom(24)) + "'"
+config = open("config.cfg", "w")
+
+standard = """DEBUG = True
+SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'"""
+
+keyVar = 'SECRET_KEY = ' + key
+
+print keyVar
+
+config.write(standard + '\n' + keyVar)
+config.close()
