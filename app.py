b68baf94ad48f1269ac9e59d3a4747bc5eb3bbad
Grant Regimbal <regimbal.grant@gmail.com>
2013-05-08 21:41:16 -0700

    Adding time and date

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-#Flask Imports
+
+#Vodka Imports
 from flask import Flask, render_template
 from pony.orm import *
+import arrow
+
+#Time & Date
+utc = arrow.utcnow()
+local = utc.to('US/Pacific')
 
 app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
-db = Database('sqlite', ':memory:')
+db = Database('sqlite', 'db.sqlite',create_db=True)
 
 #Blueprint imports
 from controllers.page import index
