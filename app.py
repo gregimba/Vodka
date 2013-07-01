a225aafd44cb01324949130671b32238f76e8430
gregimba <regimbal.grant@gmail.com>
2013-06-30 21:47:56 -0700

    version bump and cleanup

--- app.py
+++ app.py
@@ -2,11 +2,6 @@
 #Vodka Imports
 from flask import Flask, render_template
 from pony.orm import *
-import arrow
-
-#Time & Date
-utc = arrow.utcnow()
-local = utc.to('US/Pacific')
 
 app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
