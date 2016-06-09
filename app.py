08a3fc721116b9f9fa779dccf31c4404efa4d6ed
Grant Regimbal <regimbal.grant@gmail.com>
2016-06-08 20:09:13 -0700

    auth

--- app.py
+++ app.py
@@ -26,5 +26,26 @@ def index():
 def about():
 	return render_template("about.html")
 
+@app.route("/login",methods=['GET','POST'])
+def login():
+	if request.method == 'POST':
+		if request.form['USERNAME'] != app.config['USERNAME']:
+			error = "you fucked up"
+		elif request.form['PASSWORD'] != app.config['PASSWORD']:
+			error = "you fucked up"
+		else:
+			session['logged_in'] = True
+			flash("you were logged in")
+			redirect(url_for('index'))
+		return render_template("login.html",error=error)
+	else:
+		return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('index'))
+
 if __name__ == "__main__":
 	app.run()
